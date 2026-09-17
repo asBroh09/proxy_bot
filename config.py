f0c47f3b119b1ff54f2e7268a9c5c0b36c07a0e7
asBroh09 <jahd88379@gmail.com>
2026-09-17 09:25:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # config.py
 
 # BotFather থেকে নেওয়া টোকেন
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "7965662511:AAGxCU-xI9sJpfFD5YCBFSl043LEeK9Fb8I"
 
 # শুধু এই ID গুলো কমান্ড দিতে পারবে (নিজের Telegram ID দিন)
 # নিজের ID জানতে @userinfobot এ /start দিন
 ADMIN_IDS = [
-    123456789,   # আপনার ID
+    8524951580,   # আপনার ID
     # 987654321, # আরও অ্যাডমিন লাগলে যোগ করুন
 ]
 
